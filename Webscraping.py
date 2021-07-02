96e103053ad11ffe2f185dd85b2193303760bdb6
reagan olguin <olguinreagan88@gmail.com>
2021-07-02 09:35:44 -0500

    Made an array containing all of the rankings for each book. Made some of the code prettier

--- Webscraping.py
+++ Webscraping.py
@@ -1,3 +1,8 @@
+"""
+This file is used to webscrape the top books from the year 2021 on the GoodReads sight
+using selenium
+"""
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,27 +18,22 @@ driver.get('https://www.goodreads.com/book/popular_by_date/2021')
 
 time.sleep(5)
 
-for i in range(50):
+# Setting up the page so that all the top books included in 2021 can be scraped 
+#Pressing the "load more books" button as many times as needed
+while True:
     time.sleep(10)
     try:
-        print("try",i)
         Button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div/main/div[2]/div[1]/div[3]/div/div/button/span')))
         Button.click()
     except: 
         try:
-            print("except",i)
             Button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div/div[1]/div')))
             Button.click()
         except:
             break 
 
-       
-   
- 
-    
-
- 
+# grabbing all the rankings of each book
 ranking = driver.find_elements_by_css_selector('h2.Text.Text__h2.Text__italic.Text__subdued')
+ranking_list = [i.text for i in ranking]
+print(ranking_list)
 
-for i in ranking: 
-    print(i.text)
